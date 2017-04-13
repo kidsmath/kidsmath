7cf7904f9374310507e375819b8509ecf0e25a2e
Ming <ming.c@live.com>
2017-04-13 12:37:51 -0700

    Change browser history to hash history since github pages does not support browser history

--- src/About.js
+++ src/About.js
@@ -1,5 +1,14 @@
-const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
-);
+import React from 'react';
+
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>About</h2>
+            </div>
+        );
+    }
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import {
-    BrowserRouter as Router,
-    Route
+  HashRouter,
+  Route
 } from 'react-router-dom';
+
 import Home from './Home';
 import Arithmetic from './Arithmetic';
+import About from './About';
 
 class App extends Component {
     render() {
         return (
-            <Router>
+            <HashRouter>
                 <div>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/arithmetic" component={Arithmetic}/>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/arithmetic' component={Arithmetic} />
+                    <Route path='/about' component={About} />
                 </div>
-            </Router>
+            </HashRouter>
         );
     }
 }
