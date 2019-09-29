bf58f307aa9750d571f0f4352dab7c9e55f3397f
Ming <mingchen@users.noreply.github.com>
2019-09-29 13:05:40 -0700

    Reformat code

--- src/QuestionList.js
+++ src/QuestionList.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Question from './Question';
 
 export default class QuestionList extends React.Component {
-    render() {
-        const questionList = this.props.questions.map((question, index) => {
-            return <Question {...question} key={index}/>;
-        });
-        return <div>{questionList}</div>;
-    }
+  render() {
+    const questionList = this.props.questions.map((question, index) => {
+      return <Question { ...question } key = { index }
+      />;
+    });
+    return <div> { questionList } </div>;
+  }
 }
