e81da0abe167ea20ad1346f6a29c3115b0cebc37
Ming <mingchen@users.noreply.github.com>
2019-09-29 12:42:40 -0700

    Add x and /

--- src/Arithmetic.js
+++ src/Arithmetic.js
@@ -5,22 +5,30 @@ import './App.css';
 //import logo from './logo.svg';
 
 class Arithmetic extends React.Component {
-    render() {
-        const questions = new QuestionGenerator().generate(20, ['+', '-'], 5, 30);
-        return (
-            <div className="App">
-                {/* <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-
-                    <h2>Kids Math</h2>
-
-                </div>
-                <div> */}
-                    <form><QuestionList questions={questions}/></form>
-                {/* </div> */}
-            </div>
-        );
+  render() {
+    // let questions = [];
+    let questions = [];
+    let generator = new QuestionGenerator();
+    for (let i = 0; i < 20; i++) {
+      questions = questions.concat(generator.generate(1, ['+', '-'], 10, 90));
+      questions = questions.concat(generator.generate(1, ['*', '/'], 2, 9));
     }
+    // const questions = new QuestionGenerator().generate(20, ['+', '-'], 5, 30);
+    return ( <
+      div className = "App" > {
+        /* <div className="App-header">
+                            <img src={logo} className="App-logo" alt="logo"/>
+
+                            <h2>Kids Math</h2>
+
+                        </div>
+                        <div> */
+      } <
+      form > < QuestionList questions = { questions }
+      /></form > { /* </div> */ } <
+      /div>
+    );
+  }
 }
 
 export default Arithmetic;
--- src/QuestionGenerator.js
+++ src/QuestionGenerator.js
@@ -1,56 +1,65 @@
 const Parser = require('expr-eval').Parser;
 
+const opMap = {
+  '+': '+',
+  '-': '-',
+  '*': 'x',
+  '/': '÷'
+};
+
 export default class QuestionGenerator {
-    /**
-     * Generate integer in range [min, max]
-     *
-     * @param min
-     * @param max
-     * @returns {int}   integer in range [min, max]
-     */
-    static generateInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  /**
+   * Generate integer in range [min, max]
+   *
+   * @param min
+   * @param max
+   * @returns {int}   integer in range [min, max]
+   */
+  static generateInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-    generate(count = 10, ops = ['+', '-'], min = 0, max = 20) {
-        let questions = [];
-        let i = 0;
-        while (i < count) {
-            let lhs = QuestionGenerator.generateInt(min, max);
-            let rhs = QuestionGenerator.generateInt(min, max);
-            const op = ops[QuestionGenerator.generateInt(0, ops.length - 1)];
-            if (op === '-') {
-                while (lhs === 0) {
-                    // prevent 0 - 0
-                    lhs = QuestionGenerator.generateInt(min, max);
-                }
-
-                while (rhs > lhs) {
-                    // kids may not understand `3 - 5`,
-                    // need re-generate rhs to let rhs <= lhs
-                    rhs = QuestionGenerator.generateInt(min, max);
-                }
-            }
-
-            const expr = lhs + op + rhs;
-            const result = Parser.evaluate(expr);
-            // console.log('expr: ' + expr + ' = ' + result);
-
-            if (result === 0) {
-                // skip simple question. e.g. 3 - 3 = 0
-                continue;
-            }
-
-            questions.push({
-                lhs,
-                op,
-                rhs,
-                result
-            });
-
-            i += 1;
+  generate(count = 10, ops = ['+', '-'], min = 0, max = 20) {
+    let questions = [];
+    let i = 0;
+    while (i < count) {
+      let lhs = QuestionGenerator.generateInt(min, max);
+      let rhs = QuestionGenerator.generateInt(min, max);
+      const op = ops[QuestionGenerator.generateInt(0, ops.length - 1)];
+      if (op === '-') {
+        while (lhs === 0) {
+          // prevent 0 - 0
+          lhs = QuestionGenerator.generateInt(min, max);
         }
 
-        return questions;
+        while (rhs > lhs) {
+          // kids may not understand `3 - 5`,
+          // need re-generate rhs to let rhs <= lhs
+          rhs = QuestionGenerator.generateInt(min, max);
+        }
+      } else if (op === '/') {
+        lhs = lhs * rhs;
+      }
+
+      const expr = lhs + op + rhs;
+      const result = Parser.evaluate(expr);
+      // console.log('expr: ' + expr + ' = ' + result);
+
+      if (result === 0) {
+        // skip simple question. e.g. 3 - 3 = 0
+        continue;
+      }
+
+      questions.push({
+        lhs,
+        op: opMap[op],
+        rhs,
+        result
+      });
+
+      i += 1;
     }
+
+    return questions;
+  }
 }
